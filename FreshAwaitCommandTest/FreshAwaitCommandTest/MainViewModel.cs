3d95848946440afc2ba5fb15c087b2a6411b3460
Divikiran Ravela <divikiran.ravela@kmart.com.au>
2019-02-11 14:53:49 +1100

    FreshMVVM - FreshAwaitCommand

---
+++ FreshAwaitCommandTest/FreshAwaitCommandTest/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using FreshMvvm;
+using Xamarin.Forms;
+using System.ComponentModel;
+
+namespace FreshAwaitCommandTest
+{
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
+    public class MainViewModel : FreshBasePageModel
+    {
+        //Commands
+        public ICommand StepCommand { get; set; }
+        public FreshAwaitCommand StepFreshCommand { get; set; }
+
+
+        public string NumberText { get; set; }
+        private int _incrementor = 0;
+        public string Title { get; set; }
+
+
+        public MainViewModel()
+        {
+            Title = "FreshAwaitCommand";
+            NumberText = "0";
+            StepCommand = new Command(async (obj) =>
+            {
+                await IncrementNumber();
+            });
+
+            StepFreshCommand = new FreshAwaitCommand(async (obj) =>
+            {
+                await IncrementNumber();
+                obj.SetResult(true);
+            });
+        }
+
+        private async Task IncrementNumber()
+        {
+            await Task.Delay(3000);
+            NumberText = _incrementor++.ToString();
+        }
+    }
+}
